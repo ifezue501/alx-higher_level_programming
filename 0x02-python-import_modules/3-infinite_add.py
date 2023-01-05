678672779d0265db47fa53290f6980d0ba8b4b1c
ifezue501 <flasysexy501@yahoo.com>
2023-01-05 12:10:53 -0800

    update Readme

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    argc = len(sys.argv)
-    argv = sys.argv
-    res = 0
-
-    for i in range(1, argc):
-        res = res + int(argv[i])
-
-        print(res)
+    if len(sys.argv) == 1:
+        print(0)
+    elif len(sys.argv) == 2:
+        print(sys.argv[1])
+    else:
+        sum = 0
+        for i in range(1, len(sys.argv)):
+            a = int(sys.argv[i])
+            sum = sum + a
+        print(sum)
