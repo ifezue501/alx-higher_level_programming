5aa2ffd4bcad954936be56ac1e08bb11cb92342d
ifezue501 <flasysexy501@yahoo.com>
2023-04-11 11:25:51 -0700

    3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,2 +1,2 @@
 #!/usr/bin/node
-console.log('C is fun\nPython is cool\nJavaScript is amazing'); // print three lines of text using console.log() 
+console.log('C is fun\nPython is cool\nJavaScript is amazing'); 
