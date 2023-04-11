1210cb0439ec01c42416e06521d05253955ee551
ifezue501 <flasysexy501@yahoo.com>
2023-04-11 10:10:35 -0700

    First constant, first print

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// Define a constant variable called "myVar" with the value "JavaScript is amazing"
+const myVar = "JavaScript is amazing";
+// Use console.log() to print the value of myVar to the console
+console.log(myVar);
