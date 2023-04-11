a42cb4f3b480d3a09e9805c7d3ce1b018363ba93
ifezue501 <flasysexy501@yahoo.com>
2023-04-11 13:57:10 -0700

    Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,15 +1,18 @@
 #!/usr/bin/node
-// Define the factorial function
-function factorial(n) {
-  // If n is 0 or NaN, return 1
-  if (n === 0 || isNaN(n)) {
+// Get the first argument passed on the command line
+const numb = process.argv[2];
+
+// Define the recursive function to calculate the factorial
+function factorial (numb) {
+  // Check if the argument is less than or equal to 1, or not a number
+  if (numb <= 1 || isNaN(numb)) {
+    // If it is, return 1 as the factorial of 0 or 1 is 1
     return 1;
   } else {
-    // Otherwise, recursively compute the factorial of n-1 and multiply by n
-    return n * factorial(n - 1);
+    // If it is not, return the number multiplied by the factorial of (number - 1)
+    return numb * factorial(numb - 1);
   }
 }
-// Call the factorial function with the command line argument
-const arg = Number(process.argv[2]); // Convert the command line argument to a number
-const result = factorial(arg); // Compute the factorial of the argument
-console.log(result); // Print the result
+
+// Call the factorial function and print the result to the console
+console.log(factorial(numb)); 
