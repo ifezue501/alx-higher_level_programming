fc9bedc558868c89763676640d6e26caab4b0b75
ifezue501 <flasysexy501@yahoo.com>
2023-04-12 19:07:56 -0700

    Factor index

--- 0x13-javascript_objects_scopes_closures/100-data.js
+++ 0x13-javascript_objects_scopes_closures/100-data.js
@@ -1,3 +1,2 @@
-#!/usr/bin/node
-// Export an array called `list` which contains the values [1, 2, 3, 4, 5]
-exports.list = [1, 2, 3, 4, 5];
+#!/usr/bin/node 
+exports.list = [1, 2, 3, 4, 5]; // Export an array called `list` which contains the values [1, 2, 3, 4, 5] 
