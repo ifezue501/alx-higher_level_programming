4d55964ee7f47b51adcca9a24c9edd29d11acac7
ifezue501 <flasysexy501@yahoo.com>
2023-04-22 09:52:10 -0700

    Factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
+
 // Import the 'list' array from the module in '100-data.js'
 const list = require('./100-data.js').list;
-// Log the original 'list' array to the console
-console.log(list);
+
 // Create a new array by calling 'map()' on 'list' and multiplying each element by its index,
 // then log the new array to the console
-console.log(list.map((x, xI) => x * xI)); 
+const newList = list.map((x, i) => x * i);
+
+console.log('Original list:', list);
+console.log('New list:', newList); 
