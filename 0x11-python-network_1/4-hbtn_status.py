e6123ba74e17cb201b60c3d26f39047e64fed95a
ifezue501 <flasysexy501@yahoo.com>
2023-04-29 06:39:50 -0700

    fixed the errors

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,9 +7,6 @@ import requests
 
 if __name__ == "__main__":
     r = requests.get("https://alx-intranet.hbtn.io/status")
-    if r.status_code == 200:
-        print("Body response:")
-        print("\t- type: {}".format(type(r.content)))
-        print("\t- content: {}".format(r.content))
-    else:
-        print("Request failed with status code:", r.status_code)
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
