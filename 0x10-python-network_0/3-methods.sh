81345b95d2ddd1fb7ec3ac5c671eb16cb120a500
ifezue501 <flasysexy501@yahoo.com>
2023-04-30 11:51:09 -0700

    fixed errors

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send an OPTIONS request to the URL and display the allowed HTTP methods
-curl -sI -X OPTIONS "$l" | grep "Allow" | cut -d' ' -f 2-
+# Send an OPTIONS, HEAD, and PUT request to the URL and display the allowed HTTP methods
+curl -sI -X OPTIONS,HEAD,PUT "$1" | grep "Allow" | cut -d' ' -f 2-
