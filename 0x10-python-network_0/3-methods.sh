853d76ab7b3e226b8cb839447bfb8efe195975b2
ifezue501 <flasysexy501@yahoo.com>
2023-04-30 11:36:50 -0700

    cURL only methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Send a HEAD request to the URL and display the allowed HTTP methods
-curl -sI "$1" | grep "Allow: " | cut -d' ' -f 2-
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
